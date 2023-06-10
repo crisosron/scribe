99572c456b4821f7b43feb97379e7004041f7965
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-10 23:55:58 +1200

    Use Open Sans font

--- scribe-frontend/app/layout.tsx
+++ scribe-frontend/app/layout.tsx
@@ -1,10 +1,10 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Open_Sans as OpenSans } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const font = OpenSans({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Scribe',
+  title: 'Create Next App',
 }
 
 export default function RootLayout({
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   )
 }
