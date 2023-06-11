17279c7f1aac500440a5052a6e22933174c435bf
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-11 16:21:51 +1200

    Add sidebar resizing controls

--- scribe-frontend/components/sidebar/resizer.tsx
+++ scribe-frontend/components/sidebar/resizer.tsx
@@ -1,6 +1,12 @@
-const Resizer = () => { 
+type Props = {
+  onMouseDown: (e: any) => void
+};
+
+const Resizer = ({ onMouseDown }: Props) => { 
   return (
-    <div className="resize-x grow-0 shrink-0 justify-self-end cursor-col-resize hover:w-[3px] hover:bg-slate-400 basis-[6px] "/>
+    <div
+      onMouseDown={onMouseDown}
+      className="resize-x grow-0 shrink-0 justify-self-end cursor-col-resize hover:w-[1px] hover:bg-slate-400 basis-[6px] transition" />
   )
 }
 
--- scribe-frontend/components/sidebar/sidebar.tsx
+++ scribe-frontend/components/sidebar/sidebar.tsx
@@ -1,11 +1,46 @@
+'use client'
+
+import { useState, useRef, useCallback, useEffect } from 'react';
 import Resizer from "./resizer";
 import Content from "./content";
 
 const Sidebar = () => {
+  const sidebarRef = useRef<HTMLDivElement>(null);
+  const [isResizing, setIsResizing] = useState(false);
+  const [sidebarWidth, setSidebarWidth] = useState(200);
+  
+  const toggleResizing = () => {
+    setIsResizing((prev) => !prev);
+  }
+
+  const resize = useCallback((mouseEvent: MouseEvent) => {
+    if (isResizing && sidebarRef.current) { 
+      const newWidth = mouseEvent.clientX - sidebarRef.current.getBoundingClientRect().left 
+      setSidebarWidth(newWidth);
+    }
+  }, [isResizing])
+
+  useEffect(() => { 
+    window.addEventListener('mousemove', resize);
+    window.addEventListener('mouseup', toggleResizing);
+
+    return () => { 
+      window.removeEventListener('mousemove', resize);
+      window.removeEventListener('mouseup', toggleResizing);
+    }
+  }, [resize])
+
   return (
-    <div className="flex-1 min-h-screen min-w-[150px] max-w-screen md:max-w-[300px] border border-white grow-0 shrink-0 flex basis-[6px]">
+    <div
+      ref={sidebarRef}
+      style={{ width: sidebarWidth }}
+      className="min-h-screen min-w-[150px] max-w-screen md:max-w-[300px] border border-white grow-0 shrink-0 flex"
+      onMouseDown={(e) => e.preventDefault() }
+    >
       <Content />
-      <Resizer />
+      <Resizer
+        onMouseDown={(e) => { e.preventDefault(); toggleResizing(); }}
+      />
     </div>
   )
 }
