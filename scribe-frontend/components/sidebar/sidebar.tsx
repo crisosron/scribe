2472e7939d1af268e489c086b961141dcda35812
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-11 23:08:42 +1200

    Add sliding animation on sidebar toggle

--- scribe-frontend/components/sidebar/sidebar.tsx
+++ scribe-frontend/components/sidebar/sidebar.tsx
@@ -3,12 +3,13 @@
 import { useState, useRef, useCallback, useEffect } from 'react';
 import Resizer from "./resizer";
 import Content from "./content/content";
-import SidebarContextProvider, { useSidebarContext } from '@/lib/contexts/sidebar-context';
+import { useSidebarContext } from '@/lib/contexts/sidebar-context';
+import { motion } from 'framer-motion'
 
 const Sidebar = () => {
   const sidebarRef = useRef<HTMLDivElement>(null);
   const [isResizing, setIsResizing] = useState(false);
-  const [sidebarWidth, setSidebarWidth] = useState(200);
+  const [sidebarWidth, setSidebarWidth] = useState(250);
   const { showSidebar } = useSidebarContext();
 
   // Note that a 'toggleResize' fn isn't implemented as 'toggleResize' will trigger every time the
@@ -38,24 +39,25 @@ const Sidebar = () => {
     }
   }, [resize])
 
-  useEffect(() => { 
-    console.log('showSidebar has changed to: ', showSidebar);
-  }, [showSidebar])
-
   return (
-      <div
-        ref={sidebarRef}
-        style={{ width: sidebarWidth }}
-        className="min-h-screen min-w-[150px] max-w-screen md:max-w-[300px] grow-0 shrink-0 flex bg-white-200 dark:bg-soft-black-100"
-        onMouseDown={(e) => e.preventDefault() }
-      >
+    <motion.div
+      animate={{
+        width: isResizing ? sidebarWidth : showSidebar ? sidebarWidth : 50
+      }}
+      transition={{
+        type: "tween",
+        duration: 0.1
+      }}
+      ref={sidebarRef}
+      className="min-h-screen min-w-[50px] max-w-screen md:max-w-[400px] grow-0 shrink-0 flex bg-white-200 dark:bg-soft-black-100"
+      onMouseDown={(e: MouseEvent) => e.preventDefault() }
+    >
       <Content />
       <Resizer
         onMouseDown={(e) => { e.preventDefault(); startResizing(); }}
         active={ isResizing }
       />
-      
-      </div>
+    </motion.div>
   )
 }
 
