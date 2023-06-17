d27cfd1a074b645b3f56e4242c2e6e6c5070a356
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-17 18:34:34 +1200

    Add hover options to FolderItem component

--- scribe-frontend/components/sidebar/content/folder-item.tsx
+++ scribe-frontend/components/sidebar/content/folder-item.tsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useState, MouseEvent } from "react";
 import classNames from "classnames";
 import ExpandIcon from "@/components/svg-components/expand-icon/expand-icon";
 import FolderIcon from "@/components/svg-components/folder-icon/folder-icon";
 import DocumentBar from "./document-bar";
 import { AnimatePresence } from "framer-motion";
+import DotMenuIcon from "@/components/svg-components/dot-menu-icon/dot-menu-icon";
 
 const FolderItem = () => {
   const [opened, setOpened] = useState(false);
   const [hovered, setHovered] = useState(false);
+  const [showMoreOptions, setShowMoreOptions] = useState(false);
 
   const toggleOpened = () => {
     setOpened((prev) => !prev);
@@ -15,6 +17,11 @@ const FolderItem = () => {
 
   const toggleHovered = () => {
     setHovered((prev) => !prev);
+  };
+
+  const toggleShowMoreOptions = (e: MouseEvent) => {
+    e.stopPropagation();
+    setShowMoreOptions((prev) => !prev);
   };
 
   return (
@@ -31,13 +38,23 @@ const FolderItem = () => {
         </div>
         <div className="flex">
           {hovered && (
-            <ExpandIcon
-              width={20}
-              height={20}
-              className={classNames("rotate-[270deg] transition", {
-                "rotate-[360deg]": opened,
-              })}
-            />
+            <>
+              <div
+                className="hover:bg-white-250 dark:hover:bg-white-700 rounded-sm transition mr-2"
+                onClick={toggleShowMoreOptions}
+              >
+                <DotMenuIcon width={20} height={20} />
+              </div>
+              <div className="hover:bg-white-250 dark:hover:bg-white-700 rounded-sm transition">
+                <ExpandIcon
+                  width={20}
+                  height={20}
+                  className={classNames("rotate-[270deg] transition", {
+                    "rotate-[360deg]": opened,
+                  })}
+                />
+              </div>
+            </>
           )}
         </div>
       </div>
