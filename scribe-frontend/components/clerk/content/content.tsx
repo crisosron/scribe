ff3276c3e25c9280588d277c78d892b226833bfb
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-06-25 17:55:14 +1200

    Tweak clerk styling and positioning

--- scribe-frontend/components/clerk/content/content.tsx
+++ scribe-frontend/components/clerk/content/content.tsx
@@ -1,6 +1,6 @@
 const Content = () => {
   return (
-    <div className="sm:min-w-[200px] md:min-w-[40%] min-h-[200px] bg-white-100 dark:bg-soft-black-100 shadow-2xl rounded-lg p-4">
+    <div className="min-w-[80%] md:min-w-[65%] lg:min-w-[40%] min-h-[200px] bg-white-100 dark:bg-soft-black-100 shadow-2xl rounded-lg p-4">
       Content
     </div>
   );
--- scribe-frontend/components/clerk/overlay.tsx
+++ scribe-frontend/components/clerk/overlay.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 const Overlay = ({ children }: Props) => {
   return (
-    <div className="absolute w-[100vw] h-[100vh] z-[100] bg-soft-black-600/25 dark:bg-soft-black-600/50 flex justify-center items-center">
+    <div className="absolute w-[100vw] h-[100vh] z-[100] bg-soft-black-600/25 dark:bg-soft-black-600/50 flex justify-center items-start pt-4 md:pt-28">
       {children}
     </div>
   );
