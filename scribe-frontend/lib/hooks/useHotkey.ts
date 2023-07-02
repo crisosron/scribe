99a9b4bbb18b74b0deff4afc8f8e7283f598caba
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-07-02 23:01:33 +1200

    Fix stale closure issue

--- scribe-frontend/lib/hooks/useHotkey.ts
+++ scribe-frontend/lib/hooks/useHotkey.ts
@@ -11,7 +11,9 @@ type Parameters = {
 
 const useHotkey = ({ targetControlKeys, targetActionKey, callback }: Parameters) => {
   const [heldControlKeys, setHeldControlKeys] = useState<string[]>([]);
-  const handleActionKeyRef = useRef<(e: KeyboardEvent | undefined) => void>();
+  const handleKeyEventRef = useRef<(e: KeyboardEvent, eventType: EVENT_TYPE) => void>(
+    () => {} 
+  );
 
   const handleControlKeyEvent = (
     e: KeyboardEvent,
@@ -27,58 +29,30 @@ const useHotkey = ({ targetControlKeys, targetActionKey, callback }: Parameters)
     }
   };
 
-  const handleKeyEvent = (e: KeyboardEvent, eventType: EVENT_TYPE) => {
-    console.log('Handle key event heldKeys: ', heldControlKeys);
+  const handleKeyEvent = useCallback((e: KeyboardEvent, eventType: EVENT_TYPE) => {
     if(VALID_CONTROL_KEYS.includes(e.key) && targetControlKeys && targetControlKeys.includes(e.key)) handleControlKeyEvent(e, eventType)
     else if(VALID_ACTION_KEYS.includes(e.key) &&
       targetActionKey === e.key &&
-      handleActionKeyRef.current) {
+      heldControlKeys.some((key) => targetControlKeys?.includes(key))) {
       e.preventDefault();
-      handleActionKeyRef.current(e);
+      callback(e);
     }
-    
-  }
-
-
-  // const handleKeyEvent = useCallback((e: KeyboardEvent, eventType: EVENT_TYPE) => {
-  //   console.log('Handle key event heldKeys: ', heldControlKeys);
-  //   if(VALID_CONTROL_KEYS.includes(e.key) && targetControlKeys && targetControlKeys.includes(e.key)) handleControlKeyEvent(e, eventType)
-  //   else if(VALID_ACTION_KEYS.includes(e.key) &&
-  //     targetActionKey === e.key &&
-  //     handleActionKeyRef.current) {
-  //     e.preventDefault();
-  //     handleActionKeyRef.current(e);
-  //   }
-    
-  // }, [heldControlKeys])
-
-  // useLayoutEffect(() => {
-  //   console.log('useLayoutEffect heldControlKeys changed');
-  //   handleKeyEventRef.current = handleKeyEvent;
-
-  // }, [heldControlKeys])
+  }, [heldControlKeys, targetActionKey, targetControlKeys, callback])
 
   // Employ the callback ref pattern so the latest version of the callback is always called
   //
-  // Without this, the only way to get the callback to use the latest state values would be to add
+  // Without this, the only way to get the handler to use the latest state values would be to add
   // the handleKeyEvent as a dep of the useEffect that sets up the event listeners on the window
   // object, which in turn will allow the callback to be called with up-to-date states. But by doing
-  // this, there is a risk of adding multiple duplicate event listener to the window which will lead
+  // this, there is a risk of adding multiple duplicate event listeners to the window which will lead
   // to unexpected behavior.
   useLayoutEffect(() => {
-    handleActionKeyRef.current = callback;
     handleKeyEventRef.current = handleKeyEvent;
   })
 
-  const handleKeyEventRef = useRef<any>(handleKeyEvent);
-
   useEffect(() => {
-    console.log('Held control keys changed to: ', heldControlKeys);
-  }, [heldControlKeys])
-
-  useEffect(() => {
-    const handleKeyDown = (e: KeyboardEvent) => handleKeyEvent(e, "down");
-    const handleKeyUp = (e: KeyboardEvent) => handleKeyEvent(e, "up");
+    const handleKeyDown = (e: KeyboardEvent) => handleKeyEventRef.current(e, "down");
+    const handleKeyUp = (e: KeyboardEvent) => handleKeyEventRef.current(e, "up");
 
     window.addEventListener('keydown', handleKeyDown);
     window.addEventListener('keyup', handleKeyUp);
