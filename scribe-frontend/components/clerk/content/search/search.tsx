aa1d222cc3e91005669fc65010ee675a0c8d74ed
Ron Crisostomo <rcrisostomo97@gmail.com>
2023-07-08 22:13:49 +1200

    Adjust search icon size for clerk

--- scribe-frontend/components/clerk/content/search/search.tsx
+++ scribe-frontend/components/clerk/content/search/search.tsx
@@ -11,7 +11,7 @@ const Search = () => {
   return (
     <div className="w-full h-[35px] md:h-[50px] rounded-md bg-white-200 dark:bg-soft-black-400 flex items-center">
       <div className="w-[37px] md:w-[52px] h-full flex justify-center items-center">
-        <SearchIcon className="fill-white-500 dark:fill-white-400" />
+        <SearchIcon className="h-6 w-6 md:h-[30px] md:w-[30px] fill-white-500 dark:fill-white-400" />
       </div>
       <input 
         name="search-input"
